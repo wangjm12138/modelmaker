39aa2faf95a252a3f15734b6c3939a6f5ec5632c
wangjm12138 <373629168@qq.com>
2019-05-22 19:04:12 +0800

    add ","
    	modified:   modelmaker/client/api_client.py

--- modelmaker/client/api_client.py
+++ modelmaker/client/api_client.py
@@ -117,7 +117,7 @@ class ApiClient(object):
 				LOGGER.info("Refresh the token,because token is invalid or out of indate")
 				token = authorize_by_token(username=self.configuration.username,
 													password=self.configuration.password,
-													endpoint=self.configuration.host
+													endpoint=self.configuration.host,
 													verify_ssl=self.configuration.verify_ssl)
 				self.set_default_header("Auth-Token", token)
 				http_reponse = self.__call_api(resource_path, method,
