66a940e4381aba9349f33011d773a338148b0ad6
wangjm12138 <373629168@qq.com>
2019-10-17 11:15:41 +0800

    version 2.0.1 	fix upload dir/file bug, name is error 	so version 2.0.0 will delete
    	modified:   modelmaker/__init__.py
    	modified:   modelmaker/session.py
    	modified:   setup.py

--- modelmaker/__init__.py
+++ modelmaker/__init__.py
@@ -1,6 +1,6 @@
 __project__="modelmaker"
 # The version is auto-updated. Please do not edit.
-__version__="2.0.0"
+__version__="2.0.1"
 import modelmaker.config
 import modelmaker.client
 
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name='modelmaker',
-      version='2.0.0',
+      version='2.0.1',
       description='Wangsu ai sdk',
       author='wangjm',
       author_email='wangjm2@wangsu.com',
